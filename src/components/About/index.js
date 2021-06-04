1e396584d71887f89fbff950d35d68b87105e0ef
Evan Teems <evanteems40@yahoo.com>
2021-06-04 10:50:39 -0700

    Beginning src

---
+++ src/components/About/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Me from '../../assets/images/me.png';
+function About() {
+    return(
+        <section classNmae="my-5">
+            <h1 id='about'> ABout Me! </h1>
+            <img src={Me}></img>
+            <p></p>
+        </section>
+    )
+}
